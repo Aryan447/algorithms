11af96ddfae1170aba065523e358854ab74d0e4a
Aryan447 <singharyan4477@gmail.com>
2026-03-31 18:38:51 +0530

    implement leetcode 1051

---
+++ leetcode/1051.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int heightChecker(vector<int> &heights) {
+        int ans = 0;
+        vector<int> expected = heights;
+        sort(expected.begin(), expected.end());
+
+        for (int i = 0; i < expected.size(); i++) {
+            if (expected[i] != heights[i]) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution s1;
+
+    vector<int> nums = {5, 1, 2, 3, 4};
+
+    cout << s1.heightChecker(nums) << endl;
+}
+
